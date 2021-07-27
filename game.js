f9680f5fec419f0220adb14c1525a7d4ff23ac51
Brian -Laptop <bmwilson93@gmail.com>
2021-07-27 17:59:26 -0600

    Added head and body tags to index.html Added playRound function to game.js

--- game.js
+++ game.js
@@ -1,26 +1,56 @@
-/*function computerPlay () {
-    console.log("Start computer play func")
+function computerPlay () {
     let rand = Math.floor(Math.random() * 3); //generate int 0, 1, or 2
-    console.log(rand)
+    
     // check the rand and return a String
     if (rand === 0) {
-        return "Rock"
+        return "rock"
     }
     else if (rand === 1) {
-        return "Paper"
+        return "paper"
     }
     else {
-        return "Scissors"
+        return "scissors"
     }
 }
-*/
 
-function computerPlay () {
-    console.log("computerplay test")
+function playRound (playerSelection, computerSelection) {
+    // convert the playerSelection to all lowercase
+    playerSelection = playerSelection.toLowerCase()
+    
+    // test the playerSelection against the computerSelection
+
+    // same choice
+    if (playerSelection === computerSelection) {
+        return "It's a Draw!"
+    }
+    
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "paper") {
+                return "You lose. Paper beats Rock."
+            }
+            else { // Scissors
+                return "You win! Rock beats Scissors."
+            }
+        case "paper":
+            if (computerSelection === "rock") {
+                return "You win! Paper beats Rock."
+            }
+            else { // Scissors
+                return "You lose. Scissors beats Paper."
+            }
+        case "scissors":
+            if (computerSelection === "rock") {
+                return "You lose. Rock beats Scissors."
+            }
+            else { // Paper
+                return "You win! Scissors beats Paper."
+            }
+        default: // Error with player choice
+            "There seems to be something wrong with your choice."
+    }
 }
 
-function test () {
-    console.log("test")
-    let tester = computerPlay()
-    console.log(tester)
-}
+const playerSelection = "paper"
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
