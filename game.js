ac8732387890f12eed70cb442ee6067289f2c255
Brian -Laptop <bmwilson93@gmail.com>
2021-07-27 17:22:29 -0600

    Changes to game.js Having problem with getting a fuction to work

---
+++ game.js
@@ -0,0 +1,26 @@
+/*function computerPlay () {
+    console.log("Start computer play func")
+    let rand = Math.floor(Math.random() * 3); //generate int 0, 1, or 2
+    console.log(rand)
+    // check the rand and return a String
+    if (rand === 0) {
+        return "Rock"
+    }
+    else if (rand === 1) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+}
+*/
+
+function computerPlay () {
+    console.log("computerplay test")
+}
+
+function test () {
+    console.log("test")
+    let tester = computerPlay()
+    console.log(tester)
+}
